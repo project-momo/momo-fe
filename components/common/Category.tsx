8bd6a188564bfdbd9dfe645d74ffae9e4d98a29a
Icyeong <dbcjswjs@gmail.com>
2022-12-23 14:07:54 +0900

    💄 Style: 레이아웃 CSS수정 및 추가

--- components/common/Category.tsx
+++ components/common/Category.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import iconLifestyle from './../../public/images/icon_lifestyle.png'
 
 const Layout = styled.div`
    width: 277px;
    background-color: white;
-   height: calc(100vh - 105px);
+   height: auto;
    border-radius: 15px;
    box-shadow: 0px 1px 5px 1px rgba(0, 0, 0, 0.1);
    overflow-y: auto;
+   margin: 20px;
 `
 const Ul = styled.ul`
    padding: 30px 18px;
@@ -34,7 +34,7 @@ const Category = () => {
       <Layout>
          <Ul>
             <Li>
-               <a className='active'>
+               <a href='/test' className='active'>
                   <img src='./../../assets/images/icon_lifestyle.png' />
                   <p>전체</p>
                </a>
