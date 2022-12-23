3089ed5ac514fdf7b9df318e5a37342459945638
Icyeong <dbcjswjs@gmail.com>
2022-12-23 14:10:51 +0900

    ✨ Feat: router 설정 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import Header from '../components/common/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Header/>
       <Component {...pageProps} />
     </RecoilRoot>
   );
