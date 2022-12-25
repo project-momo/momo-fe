c559df67d5eb8b40c9e69239d540e8853984e63f
Icyeong <dbcjswjs@gmail.com>
2022-12-26 05:43:12 +0900

    💄 Style: 공통 섹션 UI 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import Header from '../components/common/Header'
-import Link from 'next/link';
+import { ContentLayout, GrayLayout } from '../styles/style';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Header/>
-      <Component {...pageProps} />
-      <Link href="/">Home</Link>
-      <Link href="/Sub">Sub</Link>
+      <GrayLayout>
+         <ContentLayout>
+            <Header/>
+            <Component {...pageProps} />
+         </ContentLayout>
+         </GrayLayout>
     </RecoilRoot>
   );
 }
--- styles/style.js
+++ styles/style.js
@@ -1,26 +1,25 @@
 import styled from 'styled-components';
 
 export const GrayLayout = styled.div`
-   min-height: calc(100vh - 55px);
-   background-color: #F5F5F7;
-`
+  min-height: calc(100vh - 55px);
+  background-color: #f5f5f7;
+`;
 export const ContentLayout = styled.div`
-   max-width: 1600px;
-   margin: 55px auto 0;
-   display: flex;
-   justify-content: center;
-`
-export const CenterSectionr = styled.section`
-    background-color: white;
-    width: 790px;
-    height: 500px;
-    margin: 20px 10px;
-    flex:1;
-`
+  max-width: 1600px;
+  margin: 55px auto 0;
+  display: flex;
+  justify-content: center;
+`;
+export const CenterSection = styled.section`
+  width: 790px;
+  height: 500px;
+  margin: 20px 10px;
+  flex: 1;
+`;
 
 export const RightSection = styled.section`
-    background-color: white;
-    width: 270px;
-    height: 500px;
-    margin: 20px;
-`
+  background-color: white;
+  width: 270px;
+  height: 500px;
+  margin: 20px;
+`;
