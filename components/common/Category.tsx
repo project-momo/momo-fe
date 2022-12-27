72c70e34e8101603ea65a041398c4154681acd1f
Icyeong <dbcjswjs@gmail.com>
2022-12-27 18:26:34 +0900

    💄 Style: 기타 css 수정

--- components/common/Category.tsx
+++ components/common/Category.tsx
@@ -1,99 +1,91 @@
 import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
-// import Image from 'next/image'
-import IconAll from './../../assets/images/icon_all.svg'
-import IconLifestyle from './../../assets/images/icon_lifestyle.svg'
-import IconMedia from './../../assets/images/icon_media.svg'
-import IconEdu from './../../assets/images/icon_edu.svg'
+import IconAll from './../../assets/images/icon_all.svg';
+import IconLifestyle from './../../assets/images/icon_lifestyle.svg';
+import IconEdu from './../../assets/images/icon_edu.svg';
 
 export const Layout = styled.div`
+   width: 277px;
+   margin: 20px;
+`;
+export const Ul = styled.ul`
+   padding: 30px 18px;
    width: 277px;
    background-color: white;
    height: fit-content;
    border-radius: 15px;
    box-shadow: 0px 1px 5px 1px rgba(0, 0, 0, 0.1);
    overflow-y: auto;
-   margin: 20px;
-`
-export const Ul = styled.ul`
-   padding: 30px 18px;
-`
+   position: fixed;
+`;
 export const Li = styled.li`
- a{
-   display: flex;
-   align-items: center;
-   padding: 10px 20px;
-   border-radius: 50px;
-   cursor: pointer;
- }
- a.active{
-   background-color: #ECF4FF;
- }
- a p {
-   font-size: 16px;
-   font-weight: 700;
-   padding-left: 9px;
- }
-`
+   a {
+      display: flex;
+      align-items: center;
+      padding: 10px 20px;
+      border-radius: 50px;
+      cursor: pointer;
+   }
+   a.active {
+      background-color: #ecf4ff;
+   }
+   a p {
+      font-size: 16px;
+      font-weight: 700;
+      padding-left: 9px;
+   }
+`;
 export const Category = () => {
    return (
       <Layout>
          <Ul>
             <Li>
-
-               <Link href='/test' className='active'>
+               <Link href="/test" className="active">
                   <img src={IconAll} alt="All" />
                   <p>전체</p>
-               </a>
+               </Link>
             </Li>
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconLifestyle} alt="Lifestyle" />
+               <Link href="#" className="">
+                  <img src={IconLifestyle} alt="Lifestyle" />
                   <p>라이프스타일</p>
                </Link>
             </Li>
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconEdu} alt="edu" />
+               <Link href="#" className="">
+                  <img src={IconEdu} alt="edu" />
                   <p>교육</p>
                </Link>
             </Li>
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconAll} alt="All" />
+               <Link href="#" className="">
+                  <img src={IconAll} alt="All" />
                   <p>미디어</p>
                </Link>
             </Li>
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconAll} alt="All" />
+               <Link href="#" className="">
+                  <img src={IconAll} alt="All" />
                   <p>미디어</p>
                </Link>
             </Li>
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconAll} alt="All" />
+               <Link href="#" className="">
+                  <img src={IconAll} alt="All" />
                   <p>미디어</p>
                </Link>
             </Li>
 
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconAll} alt="All" />
+               <Link href="#" className="">
+                  <img src={IconAll} alt="All" />
                   <p>미디어</p>
                </Link>
             </Li>
             <Li>
-               <Link href="#" className=''>
-
-               <img src={IconAll} alt="All" />
+               <Link href="#" className="">
+                  <img src={IconAll} alt="All" />
                   <p>미디어</p>
                </Link>
             </Li>
