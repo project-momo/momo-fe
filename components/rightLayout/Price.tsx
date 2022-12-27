0537fd9702eedbd80760aa5b3269dee19055a09a
Ryu heekyung <beals1129@gmail.com>
2022-12-27 16:44:17 +0900

    ✨ Feat: Header

--- components/rightLayout/Price.tsx
+++ components/rightLayout/Price.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from '../../stories/Button';
+import { Button } from '../common/Button';
 import IconPrice from './../../assets/images/icon_price.svg';
 const Price = () => {
   return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import Header from '../components/common/Header'
+import Header from '../components/common/Header/Header';
 import { ContentLayout, GrayLayout } from '../styles/style';
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <GrayLayout>
-         <ContentLayout>
-            <Header isLogin={false}/>
-            <Component {...pageProps} />
-         </ContentLayout>
-         </GrayLayout>
+        <ContentLayout>
+          <Header isLogin={false} />
+          <Component {...pageProps} />
+        </ContentLayout>
+      </GrayLayout>
     </RecoilRoot>
   );
 }
