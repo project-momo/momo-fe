b055210b4135f8c60a89c414d3fe03a52f1dcd29
Ryu heekyung <beals1129@gmail.com>
2022-12-28 17:20:43 +0900

    prettier setting

--- components/common/Tag.tsx
+++ components/common/Tag.tsx
@@ -4,53 +4,48 @@ import styled from 'styled-components';
 import { mainTagList } from '../../atoms';
 
 interface TagProps {
-   label : string
-   onClick?: (e: React.MouseEvent) => void;
-} 
+  label: string;
+  onClick?: (e: React.MouseEvent) => void;
+}
 
-export const Tag = ({
-   label,
-   ...props
-}:TagProps) => {
-   const [tags, setTags] = useRecoilState(mainTagList)
+export const Tag = ({ label, ...props }: TagProps) => {
+  const [tags, setTags] = useRecoilState(mainTagList);
 
-   const Toggle = () => {
-      setActive(!active)
-    
-      if(tags.includes(label)) {
-         const newTag = tags.filter(el => el !== label)
-         setTags(newTag)
-      } else {
-         setTags(tags.concat(label))
-      }
-      console.log('스프레드',[...tags])
-   }
+  const Toggle = () => {
+    setActive(!active);
 
-   const isActive= () =>{
-     return tags.includes(label)
-   }
+    if (tags.includes(label)) {
+      const newTag = tags.filter((el) => el !== label);
+      setTags(newTag);
+    } else {
+      setTags(tags.concat(label));
+    }
+  };
 
-   const [active, setActive] = useState(isActive)
+  const isActive = () => {
+    return tags.includes(label);
+  };
 
-   console.log('왜..',tags)
-   return (
-      <TagCompo onClick={Toggle} className={active ? 'active' : ''} {...props} >
-         {label}
-      </TagCompo>
-   );
+  const [active, setActive] = useState(isActive);
+
+  return (
+    <TagCompo onClick={Toggle} className={active ? 'active' : ''} {...props}>
+      {label}
+    </TagCompo>
+  );
 };
 
 const TagCompo = styled.li`
-display: inline-block;
-   font-size: 16px;
-   padding : 5px 10px;
-   border-radius: 30px;
-   margin: 15px 10px 15px 0;
-   cursor: pointer;
-   transition:background-color 0.3s;
-   &.active{
-      background-color: #D4E6FF;
-      font-weight: 700;
-      color : #6A6FF2;
-   }
-`
+  display: inline-block;
+  font-size: 16px;
+  padding: 5px 10px;
+  border-radius: 30px;
+  margin: 15px 10px 15px 0;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  &.active {
+    background-color: #d4e6ff;
+    font-weight: 700;
+    color: #6a6ff2;
+  }
+`;
