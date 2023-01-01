0371e1da9d45ff55e4ae36454347ea1e5b602fb9
Icyeong <98375183+Icyeong@users.noreply.github.com>
2023-01-01 16:56:48 +0900

    Revert "[FE] 모임 등록 테스트 완료"

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
    reactStrictMode: true,
    swcMinify: true
 };
-module.exports = nextConfig;
 
+module.exports = nextConfig;
 const withImages = require('next-images');
 module.exports = withImages();
-
-const withTM = require('next-transpile-modules')(['date-fns']);
-module.exports = withTM({});
--- pages/Sub.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-const Sub = () => {
-  const router = useRouter();
-  console.log(router, 'router');
-  return <div>subPage</div>;
-};
-
-export default Sub;
