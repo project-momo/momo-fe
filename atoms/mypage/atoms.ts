63c3b93dccffe10be8470354310a2f5112d87355
Icyeong <dbcjswjs@gmail.com>
2023-01-06 15:51:05 +0900

    chore

--- atoms/mypage/atoms.ts
+++ atoms/mypage/atoms.ts
@@ -1,31 +1,31 @@
-import { atom } from "recoil"
+import { atom } from 'recoil';
 
 export const myProfile = atom<any>({
-    key: 'myProfile',
-    default: {point: 0}
-})
+   key: 'myProfile',
+   default: { point: 0 }
+});
 
 export const mypageHostMeetings = atom<any>({
-    key: 'mypageHostMeetings',
-    default: {content: []}
-})
+   key: 'mypageHostMeetings',
+   default: { content: [] }
+});
 
 export const mypageAttendingMeetings = atom<any>({
-    key: 'mypageAttendingMeetings',
-    default: {content: []}
-})
+   key: 'mypageAttendingMeetings',
+   default: { content: [] }
+});
 
 export const myPoint = atom<any>({
-    key: 'myPoint',
-    default: []
-})
+   key: 'myPoint',
+   default: []
+});
 
 export const modalState = atom<string>({
-    key: 'modalState',
-    default: ''
-})
+   key: 'modalState',
+   default: ''
+});
 
 export const meetingInfo = atom<any>({
-    key: 'meetingInfo',
-    default: {}
-})
+   key: 'meetingInfo',
+   default: {}
+});
