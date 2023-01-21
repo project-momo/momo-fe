d0d5faba12240fd36bd318a5f24c4eb8e4d0f414
devjindev <hyjinny00@gmail.com>
2023-01-21 12:00:44 +0900

    👷 Build: vercel 테스트 중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/globals.css';
+// import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { ContentLayout, GrayLayout } from '../styles/style';
@@ -11,6 +11,7 @@ import LoginModal from '../components/common/Modal/ModalCompo/LoginModal';
 import Header from '../components/common/Header/Header';
 import axios from 'axios';
 import Head from 'next/head';
+import GlobalStyle from '../styles/GlobalStyle';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -36,7 +37,12 @@ function MyApp({ Component, pageProps }: AppProps) {
          <Head>
             <title>모모 Momo</title>
             <link rel="icon" href="/favicon.ico" />
+            <link
+               href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@300;700&display=swap"
+               rel="stylesheet"
+            />
          </Head>
+         <GlobalStyle />
          <RecoilRoot>
             {isModalOpen ? <Modal CloseModal={CloseModal} childrens={<LoginModal />} /> : null}
             <GrayLayout>
