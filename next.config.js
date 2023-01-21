72478e92818e040509bc642369cca9b50c75ea9b
devjindev <hyjinny00@gmail.com>
2023-01-21 11:10:29 +0900

    👷 Build: varcel 테스트 중

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
-   swcMinify: true,
-   trailingSlash: true,
-   images: {
-      loader: 'akamai',
-      path: '/'
-   }
+   swcMinify: true
+   // trailingSlash: true,
+   // images: {
+   //    loader: 'akamai',
+   //    path: '/'
+   // }
 };
 module.exports = nextConfig;
 
