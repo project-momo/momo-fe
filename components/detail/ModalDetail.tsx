50023b430a8ec758179e45e72ef0b5ddeda97782
Icyeong <dbcjswjs@gmail.com>
2023-02-11 08:28:06 +0900

    test

--- components/detail/ModalDetail.tsx
+++ components/detail/ModalDetail.tsx
@@ -129,21 +129,23 @@ const ModalDetail = ({ title, dateTime, price, meetingId, setIsModalOpen }: Deta
             })
             .then(res => {
                if (res.status === 201 && res.data.amount > 0) {
-                  const fetchData = {
-                     ...res.data,
-                     successUrl: 'http://localhost:3000/payments/success',
-                     failUrl: 'http://localhost:3000/payments/fail'
-                  };
-                  tossPayments.requestPayment('카드', fetchData).catch(function (error) {
-                     if (error.code === 'USER_CANCEL') {
-                        // 결제 고객이 결제창을 닫았을 때 에러 처리
-                        alert('결제가 취소되었습니다.');
-                     } else if (error.code === 'INVALID_CARD_COMPANY') {
-                        // 유효하지 않은 카드 코드에 대한 에러 처리
-                        alert('유효하지 않은 카드입니다.');
-                     }
-                  });
+                  console.log('유료 예약 성공시 : ', res);
+                  // const fetchData = {
+                  //    ...res.data,
+                  //    successUrl: 'http://localhost:3000/payments/success',
+                  //    failUrl: 'http://localhost:3000/payments/fail'
+                  // };
+                  // tossPayments.requestPayment('카드', fetchData).catch(function (error) {
+                  //    if (error.code === 'USER_CANCEL') {
+                  //       // 결제 고객이 결제창을 닫았을 때 에러 처리
+                  //       alert('결제가 취소되었습니다.');
+                  //    } else if (error.code === 'INVALID_CARD_COMPANY') {
+                  //       // 유효하지 않은 카드 코드에 대한 에러 처리
+                  //       alert('유효하지 않은 카드입니다.');
+                  //    }
+                  // });
                } else {
+                  console.log('무료 예약 성공시 : ', res);
                   alert('예약이 완료되었습니다!');
                   setIsModalOpen(false);
                }
