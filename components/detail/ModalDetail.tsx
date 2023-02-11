5b443ec34908fa7f6c816f0d70f1bb42280e7f96
Icyeong <dbcjswjs@gmail.com>
2023-02-11 21:01:41 +0900

    test

--- components/detail/ModalDetail.tsx
+++ components/detail/ModalDetail.tsx
@@ -47,12 +47,6 @@ const ModalDetail = ({ title, dateTime, price, meetingId, setIsModalOpen }: Deta
    const [isOnFocus, setIsFocus] = useState<number | undefined>(-1);
    const [clickTime, setClickTime] = useState<any>([-1, null]);
    const [startTimeSet, endTimeSet] = clickTime;
-   const clientKey: any = process.env.NEXT_PUBLIC_CLIENT_KEY;
-   let tossPayments: any;
-
-   useEffect(() => {
-      tossPayments = loadTossPayments(clientKey);
-   }, []);
 
    const TimeListSetting = (el: number) => {
       if (endTimeSet === null) {
@@ -102,8 +96,8 @@ const ModalDetail = ({ title, dateTime, price, meetingId, setIsModalOpen }: Deta
 
    const submitMoim = async () => {
       const API_URI = process.env.NEXT_PUBLIC_API_URI;
-      // const clientKey: any = process.env.NEXT_PUBLIC_CLIENT_KEY;
-      // const tossPayments = await loadTossPayments(clientKey);
+      const clientKey: any = process.env.NEXT_PUBLIC_CLIENT_KEY;
+      const tossPayments = await loadTossPayments(clientKey);
 
       if (!isLoginState) {
          alert('로그인을 진행해주세요');
@@ -141,7 +135,7 @@ const ModalDetail = ({ title, dateTime, price, meetingId, setIsModalOpen }: Deta
                      successUrl: 'https://momo-fe-two.vercel.app/payments/success',
                      failUrl: 'https://momo-fe-two.vercel.app/payments/fail'
                   };
-                  tossPayments.requestPayment('카드', fetchData).catch(function (error: any) {
+                  tossPayments.requestPayment('카드', fetchData).catch(function (error) {
                      if (error.code === 'USER_CANCEL') {
                         // 결제 고객이 결제창을 닫았을 때 에러 처리
                         alert('결제가 취소되었습니다.');
